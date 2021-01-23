fd30a40f27468a8ec13a489404a77b82d60975c3
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-01-23 22:16:15 +0100

    Add axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "./Header";
+import { UNSPLASH_BASE_URL } from "../constants";
+import { UNSPLASH_ACCESS_KEY } from "../accesses";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${UNSPLASH_BASE_URL}/photos/?client_id=${UNSPLASH_ACCESS_KEY}`)
+      .then((res) => {
+        const getData = res.data;
+        setData(getData);
+        console.log(getData);
+      });
+  }, []);
+
   return (
-    <Header name="My React Gallery"/>
+    <>
+      <Header name="My React Gallery" />
+      <div>
+        {data.map((item, index) => (
+          <p>{item.id}</p>
+        ))}
+      </div>
+    </>
   );
 }
 
